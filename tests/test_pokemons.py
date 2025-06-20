53065d42bc9151dacfc41de312ba2358bf347f21
Alex_Obydenov <wanteruga01@yandex.ru>
2025-06-20 23:54:18 +0300

    Приведено к домашнему заданию

--- tests/test_pokemons.py
+++ tests/test_pokemons.py
@@ -7,15 +7,9 @@ header = {'content-Type' : 'application/json', 'trainer_token':token}
 trainer_id = '35816'
 
 def test_status_code():
-    response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : trainer_id})
+    response = requests.get(url = f'{URL}/trainers', params = {'sort' : 'desc_level'})
     assert response.status_code == 200
 
-def test_part_of_response():
-    response_get = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : trainer_id})
-    assert response_get.json()["data"][0]['name'] == 'Бульбазавр'
-
-
-@pytest.mark.parametrize('key, value', [('name', 'Бульбазавр'), ('trainer_id', trainer_id), ('id', '340219')])
-def test_parametrize(key, value ):
-    response_parametrize = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : trainer_id})
-    assert response_parametrize.json()["data"][0][key] == value
+def test_trainer_info():
+    response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : trainer_id})
+    assert response_get.json()["data"][0]['id'] == trainer_id
